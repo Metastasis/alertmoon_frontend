1572408cb8f567c0a207702565ec855039ca2aa0
d.rodkin <d.rodkin@tinkoff.ru>
2022-10-08 14:09:45 +0300

    Пофикшен цикличный запрос

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const Home = () => {
         // Redirect to login page
         return router.push(edgeConfig.basePath + "/ui/login")
       })
-  })
+  }, [])
 
   if (!session) {
     // Still loading
